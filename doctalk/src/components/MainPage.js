e3f0b721fb785f101e9679a724780ec2bd73b1bd
hu_chong_xern <chongxern@gmail.com>
2024-01-27 14:59:39 +0800

    Adding more features

--- doctalk/src/components/MainPage.js
+++ doctalk/src/components/MainPage.js
@@ -1,25 +1,33 @@
 import { useEffect, useState } from 'react'
 import NavBar from './NavBar';
+import '../styles/MainPage.css'
+
 
 export default function MainPage() {
-    const [recentPosts, setRecentPosts] = useState([
-        { id: 1, title: 'Post 1', description: 'First!' },
-        { id: 2, title: 'Post 2', description: 'Not first'},
-        { id: 3, title: 'Post 3', description: 'But not the last'},
-        { id: 4, title: 'Post 4', description: '3A'},
-        { id: 5, title: 'Post 5', description: 'Last one for now'}
-    ]);
+    const popularPosts = [
+        { id: 1, title: 'Accidentally cut my finger', text: 'Should I see the doctor about this or let it heal itself?', op: 'uqewocrde12', image: '../media/injury images/bleeding_finger.png', comments: 15, timeAdded: '2 hrs ago', isResolved: true},
+        { id: 2, title: 'A cat scratched me', text: 'And it\'s a wild cat btw', op: 'LJMO2001', image: '../media/injury images/cat_scratch.png', comments: 0, timeAdded: 'just now', isResolved: false},
+        { id: 3, title: 'A dog bit me', text: 'This one\'s a wild dog also', op: 'LJMO2001', image: '../media/injury images/puppy.png', comments: 17, timeAdded: '5 hrs ago', isResolved: true},
+        { id: 4, title: 'Lost my nose on my way to school', text: 'How am I still breathing', op: 'tom_marvalo_riddle', image: '../media/injury images/broken_nose.png', comments: 6, timeAdded: '1 day ago', isResolved: false},
+        { id: 5, title: 'Sneezing a lot lately', text: 'Is this allergy or some cold?', op: 'snot_person', image: '../media/injury images/sneezing.png', comments: 0, timeAdded: 'just now', isResolved: false},
+        { id: 6, title: 'Lost my tooth after eating lunch', text: 'Title.', op: 'fish', image: '../media/injury images/tooth_loss.png', comments: 2, timeAdded: '3 hrs ago', isResolved: true},
+        { id: 7, title: 'Losing hair after waking up', text: 'Not again', op: 'hrq2024', image: '../media/injury images/hair_loss.png', comments: 0, timeAdded: '1 day ago', isResolved: false},
+        { id: 8, title: 'My cat is sick', text: 'For the past week actually', op: 'mickey_mouse', image: '../media/injury images/sick_pet.png', comments: 10, time_added: '1 day ago', isResolved: true}
+    ];
     return (
-        <div>
-            <NavBar />
-            <h1> Popular posts </h1>
-            <div className="popular-posts-scroll">
-                {recentPosts.map((post) => (
-                    <div key={post.id} className="popular-post">
-                        <h3> {post.title}</h3>
-                        <p> {post.description}</p>
-                    </div>
-                ))}
+        <div className='main-page'>
+            <div className="popular-posts">
+                <NavBar />
+                <div className="posts-container">
+                <h2> Popular posts </h2>
+                    {popularPosts.map((post) => (
+                        <div key={post.id} className="post">
+                            <div className="post-title"> {post.title} </div>
+                            <div className="op"> {post.op} </div>
+                            <div> {post.image && <img src={post.image} alt="Post Image" />} </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
