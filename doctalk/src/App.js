7631873266a9dcec288b21389a45be439e287241
hongyijie06 <hongyijie06@gmail.com>
2024-01-29 23:30:50 +0800

    Revert "add routes for front end"
    This reverts commit c4fe80acb8ba8eb354b125dc710a1e8d628d6c3a.

--- doctalk/src/App.js
+++ doctalk/src/App.js
@@ -1,32 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import MainPage from './components/MainPage';
-import PostPage from './components/PostPage';
-import LoginPage from './components/LoginPage';
-
-import{
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-    
-}from "react-router-dom";
 
 function App() {
   return (
-    
-    <>
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<LoginPage/>}/>
-        <Route exact path="/components/PostPage" element={<PostPage />}/>
-        <Route exact path="/components/MainPage" element={<MainPage />}/>        
-      </Routes>
-    
-      
-      
-    </Router>
-    </>
-   
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
