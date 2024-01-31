231d13294b67dc42c06ca62bee7023dff962ef97
Chong Xern <104547209+ChongXern@users.noreply.github.com>
2024-02-01 00:39:15 +0800

    Adjusted upload photo part

--- doctalk/src/App.js
+++ doctalk/src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import logo from './media/draft_logo.png'
 
 import MainPage from './components/MainPage';
+import PostPage from './components/PostPage';
 import LoginPage from './components/LoginPage';
 import UploadPage from './components/UploadPage';
 import{
@@ -15,47 +16,27 @@ import{
 
 function App() {
   return (
+    <div className='App'>
+      <UploadPage />
+    </div>
+  );
+};
+/*
+function App() {
+  return (
     <>
-    
-    
+    <div className='App'>
+      <LoginPage />
+    </div>
     <Router>
       <Routes>
         <Route exact path="/" element = {<LoginPage/>}/>
-        <Route exact path="/components/UploadPage" element = {<UploadPage/>}/>
-        <Route exact path="/components/MainPage" element = {<MainPage/>}/>
+        <Route exact path = "/components/PostPage" element = {<PostPage/>}/>
+        <Route exact path="/compunents/MainPage" element = {<MainPage/>}/>
       </Routes>
     </Router>
     </>
   );
-
-
-
-
-/*<div className='App'>
-      <UploadPage />
-  </div>
-*/
-    
-    //<h1> DocTalk! </h1>
-  
-  /*return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );*/
-}
+}*/
 
 export default App;
