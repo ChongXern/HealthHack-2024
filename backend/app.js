f66d64f789fe40feb9a7228590d4bb5bf06cb08f
ngxzs <seanng01@outlook.com>
2024-02-01 14:08:57 +0800

    Add HASH, isValid(newUser) function, /test POST view page

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ let bcrypt = require('bcrypt');
 const app = express();
 
 // listen for request
-app.listen(3000); // also returns instance of server ie http.createServer()
+app.listen(3030); // also returns instance of server ie http.createServer()
 
 // middleware & static files
 app.use(express.static('public')); //all can use public files
@@ -17,10 +17,10 @@ app.use(express.json());
 async function hashPw(password) {
     let hashPw = "";
     const salt = await bcrypt.genSalt(1);
-    console.log(salt);
+    // console.log(salt);
     hashPw = await bcrypt.hash(password, salt);
-    console.log(password + " is now: ");
-    console.log(hashPw + '\n');
+    // console.log(password + " is now: ");
+    // console.log(hashPw + '\n');
     return hashPw;
 }
 
@@ -41,7 +41,7 @@ class User {
         this.lastName = lastName;
         this.userType = userType;
         this.email = email;
-        this.password = hashPw(password);
+        this.password = password;
     }
 
     getUserId() {
@@ -127,9 +127,8 @@ let currentUser = null;
 // API endpoints
 //testing
 app.set('view engine', 'ejs')
-
 app.get('/test', (req, res) => {
-    res.render('newUser');
+    res.render('newUser', { users });
 })
 
 // Current user
@@ -160,29 +159,41 @@ app.get('/api/posts', (req, res) => {
     res.json(allPosts);
 })
 
+function isValid(newUser) { //checks repeated email, username
+    for (var i = 0; i < users.length; i++) {
+        if ((users[i].email == newUser.email)) {
+            console.log("invalid user coz repeated email" + '\n');
+            return 0;
+        } else if (users[i].username == newUser.username) {
+            console.log("invalid user coz repeated username" + '\n');
+            return 0;
+        }
+    }
+    console.log("valid user" + '\n');
+    return 1;
+}
+
 // Sign up
 app.post('/api/signup', async (req, res) => {
-    // TODO: HASH password using bcrypt
-    // const newUser = await new User( //hash pw not showing
-    //     ++userCount,
-    //     username = "test1",
-    //     firstName = "Awe",
-    //     lastName = "some",
-    //     userType = "student",
-    //     email = req.body.email,
-    //     password = req.body.password
-    // );
-    // users.push(newUser);
-    // res.redirect('/test');
+    // TODO: HASH password using bcrypt (testing purposes)
+    const newUser = new User(
+        ++userCount,
+        username = req.body.username,
+        firstName = "Awe", //replace w req.body.<property> etc
+        lastName = "some",
+        userType = "student",
+        email = req.body.email,
+        password = await hashPw(req.body.password)
+    );
+    if (isValid(newUser)) { // TODO: isValid function: checks repeated email, username, password    // }
+        users.push(newUser);
+    } else {
+        //console.log(("user invalid")); 
+        // redirect to user sign up page w error message
+    }
+    //res.redirect('/');
 
-    // actual code
-    const newUser = req.body;
-    users.push(new User(++userCount, newUser.username, newUser.firstName, newUser.lastName, newUser.userType, newUser.email, newUser.password));
-    res.redirect('/');
-    // TODO: isValid function: checks repeated email, username, password
-    //if (isValid(newUser)) { 
-
-    //}
+    res.redirect('/test'); // testing
 })
 
 // Sign in
